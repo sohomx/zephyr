91744ed79499c6ba45bc353ecb6eb2dc5a0f4b64
sohom <84140043+sohomx@users.noreply.github.com>
2023-11-08 15:13:17 +0530

    Add files via upload

---
+++ petcare/ingest.py
@@ -0,0 +1,29 @@
+import os
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.document_loaders import PyPDFLoader
+
+model_name = "BAAI/bge-large-en"
+model_kwargs = {'device': 'cpu'}
+encode_kwargs = {'normalize_embeddings': False}
+
+embeddings = HuggingFaceEmbeddings(
+    model_name = model_name,
+    model_kwargs = model_kwargs,
+    encode_kwargs = encode_kwargs
+)
+
+print("Embeddings Model Loaded........")
+
+loader = PyPDFLoader("pet.pdf")
+documents = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+texts = text_splitter.split_documents(documents)
+
+vector_store = Chroma.from_documents(
+    texts,
+    embeddings,
+    collection_metadata = {"hnsw:space": "cosine"},
+    persist_directory = "stores/pet_cosine"
+)
